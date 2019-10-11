586571e1eba06cb9ece1698ab354a8008dcc5c0c
JsKim4 <ggi4111@naver.com>
2019-10-12 00:20:10 +0900

    Hikaricp 설정 및 DataSourceTest 통과

---
+++ src/test/java/org/kjs/persistence/JDBCTests.java
@@ -0,0 +1,33 @@
+package org.kjs.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class JDBCTests {
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testConnection() {
+		try (Connection con = DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306/ggi4111", 
+				"ggi4111",
+				"kjs468844")) {
+			log.info(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+}
