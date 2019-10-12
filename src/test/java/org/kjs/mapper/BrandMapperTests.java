8004bdbd6c7aaebb5ff104d37d2ceff8c06237c8
JsKim4 <ggi4111@naver.com>
2019-10-12 11:33:05 +0900

    brandMapper test

--- src/test/java/org/kjs/mapper/BrandMapperTests.java
+++ src/test/java/org/kjs/mapper/BrandMapperTests.java
@@ -2,6 +2,8 @@ package org.kjs.mapper;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.kjs.domain.BrandVO;
+import org.kjs.domain.Criteria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -16,6 +18,47 @@ public class BrandMapperTests {
 	@Setter(onMethod_ = @Autowired)
 	private BrandMapper mapper;
 	
+	@Test
+	public void testGetTotalCount() {
+		Criteria cri = new Criteria(2,10);
+		log.info(mapper.getTotalCount(cri));
+		
+	}
+	
+	@Test
+	public void testGetListWithPage(){
+		Criteria cri = new Criteria(2,10);
+		mapper.getListWithPage(cri).forEach(brand->log.info(brand));
+	}
+	
+	
+	@Test
+	public void testGet() {
+		log.info(mapper.get(18L));
+	}
+	
+	
+	@Test 
+	public void testDelete() {
+		log.info("result : "+mapper.delete(15L));
+	}
+	
+	
+	@Test
+	public void testInsertSelectKey(){
+		BrandVO vo = new BrandVO();
+		vo.setBrandName("test Name");
+		mapper.insertSelectKey(vo);
+		log.info(vo);
+	}
+	
+	@Test 
+	public void testInsert() {
+		BrandVO vo = new BrandVO();
+		vo.setBrandName("test Name");
+		mapper.insert(vo);
+		
+	}
 	@Test 
 	public void testGetList() {
 		mapper.getList().forEach(brand->log.info("brandLog"+brand));
