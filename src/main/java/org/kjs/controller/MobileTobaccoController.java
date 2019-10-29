407cea9883fed0c2e3cabedb6f592f550db69fc5
kjs <46150254+JsKim4@users.noreply.github.com>
2019-10-29 23:33:15 +0900

    검색 추가

--- src/main/java/org/kjs/controller/MobileTobaccoController.java
+++ src/main/java/org/kjs/controller/MobileTobaccoController.java
@@ -5,13 +5,14 @@ import java.util.List;
 import org.kjs.domain.Criteria;
 import org.kjs.domain.TobaccoVO;
 import org.kjs.service.TobaccoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,11 +24,14 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @AllArgsConstructor
 public class MobileTobaccoController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MobileTobaccoController.class);
 	TobaccoService service;
 	@PostMapping(value="/list", produces = { MediaType.APPLICATION_XML_VALUE,
 			MediaType.APPLICATION_JSON_UTF8_VALUE })
-	public ResponseEntity<List<TobaccoVO>> list(@RequestBody Criteria cri) {
+	public ResponseEntity<List<TobaccoVO>> list(Criteria cri) {
 		cri.setStartIndex();
+		logger.debug(cri.toString());
 		int total = service.getTotalCount(cri);
 		return new ResponseEntity<>(service.getList(cri), HttpStatus.OK);
 	}
